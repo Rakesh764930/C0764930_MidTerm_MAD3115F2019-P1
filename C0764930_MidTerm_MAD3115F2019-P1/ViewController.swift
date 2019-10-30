d0a73f36e531ce1c7caf071d719dca5fb0ee2d27
Rakesh Kumar <rakeshkumar@192215@gmail.com>
2019-10-30 19:46:06 -0400

    Initial Commit

---
+++ C0764930_MidTerm_MAD3115F2019-P1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0764930_MidTerm_MAD3115F2019-P1
+//
+//  Created by MacStudent on 2019-10-30.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
