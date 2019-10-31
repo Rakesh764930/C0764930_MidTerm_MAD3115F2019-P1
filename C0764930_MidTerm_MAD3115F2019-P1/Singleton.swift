67a1b91ff9b0e81ce7896bfd813861766f04952b
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-10-31 17:54:36 -0400

    singleton class

---
+++ C0764930_MidTerm_MAD3115F2019-P1/Singleton.swift
@@ -0,0 +1,39 @@
+//
+//  Singleton.swift
+//  C0764930_MidTerm_MAD3115F2019-P1
+//
+//  Created by MacStudent on 2019-10-31.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class Singleton: NSObject {
+    
+    
+    var custDict:[Int:Customer]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
