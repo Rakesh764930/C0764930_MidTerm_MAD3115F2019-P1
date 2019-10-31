b703f776eb76f578bd01c70598680270e3951cb5
Rakesh kumar <Rakeshkumar92215@gmail.com>
2019-10-31 16:17:37 -0400

    username validation added

--- C0764930_MidTerm_MAD3115F2019-P1/LoginViewController.swift
+++ C0764930_MidTerm_MAD3115F2019-P1/LoginViewController.swift
@@ -9,30 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var txtUsername: UITextField!
+ //      o u t l e s t s
     
+    @IBOutlet weak var txtUsername: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
+    
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
     @IBAction func btnLogin(_ sender: UIButton)
     {
-        let unm=txtUsername.text
-        let psw=txtPassword.text
+        let usnm=txtUsername.text!
+        let pswd=txtPassword.text!
         
-        if unm=="Rks764930"
+        if usnm.count > 5
         {
-            
-        }
         
+        if usnm == "Rks764930"
+        {
+          
         
+    }
+    else{
+    let alert = UIAlertController(title: "Email Error", message: "Your Email is less than 10 charachter. Please enter a valid email", preferredStyle: .alert)
+    
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    alert.addAction(UIAlertAction(title: "Ignore", style: .destructive, handler: nil))
+    
+    self.present(alert, animated: true)
+    
     }
     
-    @IBAction func swRememberMe(_ sender: UISwitch) {
-    }
 }
 
+}
