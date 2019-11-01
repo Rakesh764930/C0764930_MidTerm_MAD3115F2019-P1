4e59982f6f00e21443de153ba6ad598d4b22a0d7
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-10-31 21:48:21 -0400

    new customer view controller outlet and  action

---
+++ C0764930_MidTerm_MAD3115F2019-P1/NewCustomerViewController.swift
@@ -0,0 +1,28 @@
+//
+//  NewCustomerViewController.swift
+//  C0764930_MidTerm_MAD3115F2019-P1
+//
+//  Created by MacStudent on 2019-10-31.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class NewCustomerViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var txtFirstName: UITextField!
+    @IBOutlet weak var txtLastName: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        }
+    
+
+    @IBAction func btnAddCust(_ sender: UIButton) {
+    }
+    
+
+}
--- C0764930_MidTerm_MAD3115F2019-P1/Singleton.swift
+++ C0764930_MidTerm_MAD3115F2019-P1/Singleton.swift
@@ -52,11 +52,11 @@ class Singleton: NSObject {
     
     func defaultCustomer()  {
 
-        var cust1 = Customer(customerID : 1, firstName : "Rakesh", lastName : "Kumar", email : "Rakeshkumar92215@gmail.com")
-        var cust2 = Customer(customerID : 2, firstName : "Ajeet", lastName : "Singh", email : "ajeets110@gmail.com")
-        var cust3 = Customer(customerID : 3, firstName : "Gurpreet", lastName : "Singh", email : "gurpreetsingh26@gmail.com")
-        var cust4 = Customer(customerID : 4, firstName : "Kuldeep", lastName : "Bamrah", email : "Kuldeepbamrah0@gmail.com")
-        var cust5 = Customer(customerID : 5, firstName : "Ritik", lastName : "Jagpal", email : "Ritikjagpal123@gmail.com")
+        let cust1 = Customer(customerID : 1, firstName : "Rakesh", lastName : "Kumar", email : "Rakeshkumar92215@gmail.com")
+        let cust2 = Customer(customerID : 2, firstName : "Ajeet", lastName : "Singh", email : "ajeets110@gmail.com")
+        let cust3 = Customer(customerID : 3, firstName : "Gurpreet", lastName : "Singh", email : "gurpreetsingh26@gmail.com")
+        let cust4 = Customer(customerID : 4, firstName : "Kuldeep", lastName : "Bamrah", email : "Kuldeepbamrah0@gmail.com")
+        let cust5 = Customer(customerID : 5, firstName : "Ritik", lastName : "Jagpal", email : "Ritikjagpal123@gmail.com")
     }
 
     func addCustomer(c:Customer){
