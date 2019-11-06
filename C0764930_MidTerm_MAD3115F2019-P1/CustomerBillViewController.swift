db1122af04f10e666d372ad1821065cd57107d39
Rakesh Kumar <Rakeshkumar92215@gmail.com>
2019-11-05 21:29:23 -0500

    new  cocoa touch class added

---
+++ C0764930_MidTerm_MAD3115F2019-P1/CustomerBillViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerBillViewController.swift
+//  C0764930_MidTerm_MAD3115F2019-P1
+//
+//  Created by MacStudent on 2019-11-05.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class CustomerBillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
