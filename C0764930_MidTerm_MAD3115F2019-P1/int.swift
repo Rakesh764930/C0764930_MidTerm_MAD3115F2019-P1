36a9092074f042949c30b7bf7007815a41d5a4a1
Rakesh kumar <Rakeshkumar@gmail.com>
2019-11-08 13:17:33 -0500

    extensions added

---
+++ C0764930_MidTerm_MAD3115F2019-P1/int.swift
@@ -0,0 +1,16 @@
+//
+//  int.swift
+//  C0764930_MidTerm_MAD3115F2019-P1
+//
+//  Created by MacStudent on 2019-11-08.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+extension Int{
+    
+    func UnitConverter() ->String {
+        return "\(self) Units"
+    }
+
+}
